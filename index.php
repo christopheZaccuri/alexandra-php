777e99aeac557e1e5eec7ad7d41df5c2dc054433
christophe <czaccuri@gmail.com>
2023-08-25 18:00:28 +0200

    add banner

--- index.php
+++ index.php
@@ -4,11 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Gloria+Hallelujah&display=swap" rel="stylesheet">
     <script src="dist/bundle.js"></script>
     <title>Document</title>
 </head>
 <body>
-    <div class="grid">
+    <div class="banner">
+        <h1>Baptême Alexandra</h1>
+    </div>
+    <div class="grid hide">
     <?php 
     $handle = opendir(dirname(realpath(__FILE__)).'/dist/img/');
     while($file = readdir($handle)){
@@ -41,5 +47,33 @@
     }
     .grid {
     margin: 0 auto;
+    }
+    .hide {
+        display: none;
+    }
+    .banner {
+        width: 100%;
+        height: 150px;
+        background-image: url('dist/img/DSCN6816.JPG');
+        background-size: cover;
+        position: relative;
+        display: flex;
+        justify-content:center;
+        align-items: center;
+    }
+    .banner:after {
+        content:"";
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 0;bottom: 0;
+        background: rgba(0,0,0,0.3);
+        z-index: 1;
+    }
+    h1 {
+        color: #fff;
+        font-family: 'Gloria Hallelujah', cursive;
+        position: relative;
+        z-index: 2;
     }
 </style>
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import Masonry from 'masonry-layout'
 
 window.onload = function() {
+  document.querySelector('.grid').classList.remove('hide')
     const elem = document.querySelector('.grid');
     const msnry = new Masonry( elem, {
       // options
